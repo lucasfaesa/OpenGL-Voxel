598ccc9bb59d1d02897ff72b31b812bba67598f5
Lucas Rios <lucasfaesa@gmail.com>
2026-01-29 22:04:15 -0300

    week 0, day 2, done!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <fstream> 
 using namespace std;
+
+class Logger{
+
+    ofstream   handle;
+
+    public:
+        Logger(const string& filename):handle(filename){
+            if(!handle.is_open()){
+                throw runtime_error("Could not open log file");
+            }
+        }
+        
+        ~Logger(){
+            cout << "Logger destroyed";
+            handle.close();
+        }
+
+        void log(const string& message){
+            if(handle.is_open()){
+                handle << message << endl;
+            }
+        }
+};
 
 int main()
 {
-    cout<< "Hello, World!" << endl;
-    int a;
-    cin>>a;
+    cout<< "Hello, log something!" << endl;
+
+    string input;
+    cin >> input;
+
+    {
+        Logger logger = Logger("yo.txt");
+        
+        logger.log(input);
+        cout << "Logged succesfully" << endl;
+    }
+
+
+    cin.get();
+
     return 0;
 }
+
