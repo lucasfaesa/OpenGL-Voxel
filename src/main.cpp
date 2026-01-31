b771a0f43ec92ff8554d7dd688e9c689ef86b7fa
Lucas Rios <lucasfaesa@gmail.com>
2026-01-31 15:04:33 -0300

    week 0, day 3. Imported GLAD and GLFW, creating empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,55 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include <fstream> 
-using namespace std;
-
-class Logger{
-
-    ofstream   handle;
-
-    public:
-        Logger(const string& filename):handle(filename){
-            if(!handle.is_open()){
-                throw runtime_error("Could not open log file");
-            }
-        }
-        
-        ~Logger(){
-            cout << "Logger destroyed";
-            handle.close();
-        }
-
-        void log(const string& message){
-            if(handle.is_open()){
-                handle << message << endl;
-            }
-        }
-};
 
 int main()
 {
-    cout<< "Hello, log something!" << endl;
+    GLFWwindow* window;
 
-    string input;
-    cin >> input;
-
-    {
-        Logger logger = Logger("yo.txt");
-        
-        logger.log(input);
-        cout << "Logged succesfully" << endl;
+    //Initialize the library
+    if(!glfwInit()){
+        std::cerr << "Failed to initalize GLFW";
+        return -1;
     }
 
+    //Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 
-    cin.get();
+    if(!window){
+        glfwTerminate();
+        return -1;
+    }
+
+    //Make the window's context current
+    glfwMakeContextCurrent(window);
+
+
+    //Loads function pointers for OpenGL
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //Loop until the user closes the window
+    while(!glfwWindowShouldClose(window)){
+        
+        //clears the window to the default color (black)
+        //GL_COLOR_BUFFER_BIT means we are clearing the color buffer, what you see
+        //on the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //swap front and back buffers
+        //Modern OpenGL uses double-buffering: you draw on the back buffer while the front
+        //buffer is displayed, then swap them to avoid flickering
+        glfwSwapBuffers(window);
+
+
+        //Processes OS and window events, like keyboard/mouse input and window closing
+        //Without htis, the window would become unresponsive or not close
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
